01c21fdb53477ea06384c6c11f6fb9cfeb2105a6
Douglas McCloskey <dmccloskey87@gmail.com>
2018-07-10 15:11:03 +0200

    build: added external imgui headers required for the gui

---
+++ external/imgui.cmake
@@ -0,0 +1,17 @@
+include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
+
+set_property (DIRECTORY PROPERTY EP_BASE Dependencies)
+
+set (DEPENDENCIES)
+
+set (IMGUI_VERSION master)
+message (STATUS "Adding ImGUI ${IMGUI_VERSION} as an external project.")
+
+ExternalProject_Add(imgui
+  GIT_REPOSITORY "https://github.com/ocornut/imgui.git"
+  GIT_TAG ${IMGUI_VERSION}
+  UPDATE_COMMAND ""
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+)
