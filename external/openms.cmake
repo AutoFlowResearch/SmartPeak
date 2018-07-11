49985e201312c9179251218824dbb2cbf2d5a70d
Douglas McCloskey <dmccloskey87@gmail.com>
2018-07-11 12:13:56 +0200

    fix: updated openms.cmake

--- external/openms.cmake
+++ external/openms.cmake
@@ -4,7 +4,7 @@ set_property (DIRECTORY PROPERTY EP_BASE Dependencies)
 
 set (DEPENDENCIES)
 
-set (CONTRIB_VERSION develop)
+set (CONTRIB_VERSION 8440071)
 message (STATUS "Adding CONTRIB ${CONTRIB_VERSION} as an external project.")
 
 ExternalProject_Add(contrib
@@ -25,9 +25,8 @@ ExternalProject_Add(openms
     GIT_REPOSITORY "https://github.com/OpenMS/OpenMS.git"
     GIT_TAG ${OPENMS_VERSION}
     CMAKE_ARGS 
-        -DCMAKE_INSTALL_PREFIX=${INCLUDE_DIR_OPENMS}
         -DPYOPENMS=OFF
-        #-DCMAKE_PREFIX_PATH="/usr/local/contrib-build/;/usr/local/contrib/;/usr/;/usr/local"
+        -DCMAKE_PREFIX_PATH=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/Source/contrib/build
         -DBOOST_USE_STATIC=OFF
         -DHAS_XSERVER=OFF
         -DWITH_GUI=OFF
