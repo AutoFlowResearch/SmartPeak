8167e2a2e3b92c486085c302009dc66106d3cdc7
Pasquale Colaianni <pasqoo@users.noreply.github.com>
2018-07-19 15:49:06 +0200

    [py to cpp] SequenceSegmentHandler (#45)
    * feat: 1st commit WIP
    
    * feat: implement class
    
    * fix: apply review comments
    
    * fix: apply review comments
    
    add const keyword in setters' arguments
    
    * chore: add unmerged class

---
+++ src/smartpeak/source/core/SequenceSegmentHandler.cpp
@@ -0,0 +1,94 @@
+// TODO: Add copyright
+
+#include <SmartPeak/core/SequenceSegmentHandler.h>
+
+namespace SmartPeak
+{
+  void SequenceSegmentHandler::clear()
+  {
+    sequence_segment_name_.clear();
+    sample_indices_.clear();
+    standards_concentrations_.clear();
+    quantitation_methods_.clear();
+    components_to_concentrations_.clear();
+  }
+
+  void SequenceSegmentHandler::setSequenceSegmentName(const std::string& sequence_segment_name)
+  {
+    sequence_segment_name_ = sequence_segment_name;
+  }
+
+  std::string& SequenceSegmentHandler::getSequenceSegmentName()
+  {
+    return sequence_segment_name_;
+  }
+
+  std::string SequenceSegmentHandler::getSequenceSegmentName() const
+  {
+    return sequence_segment_name_;
+  }
+
+  void SequenceSegmentHandler::setSampleIndices(const std::vector<size_t>& sample_indices)
+  {
+    sample_indices_ = sample_indices;
+  }
+
+  std::vector<size_t>& SequenceSegmentHandler::getSampleIndices()
+  {
+    return sample_indices_;
+  }
+
+  std::vector<size_t> SequenceSegmentHandler::getSampleIndices() const
+  {
+    return sample_indices_;
+  }
+
+  void SequenceSegmentHandler::setStandardsConcentrations(const std::vector<OpenMS::AbsoluteQuantitationStandards::runConcentration>& standards_concentrations)
+  {
+    standards_concentrations_ = standards_concentrations;
+  }
+
+  std::vector<OpenMS::AbsoluteQuantitationStandards::runConcentration>& SequenceSegmentHandler::getStandardsConcentrations()
+  {
+    return standards_concentrations_;
+  }
+
+  std::vector<OpenMS::AbsoluteQuantitationStandards::runConcentration> SequenceSegmentHandler::getStandardsConcentrations() const
+  {
+    return standards_concentrations_;
+  }
+
+  void SequenceSegmentHandler::setQuantitationMethods(const std::vector<OpenMS::AbsoluteQuantitationMethod>& quantitation_methods)
+  {
+    quantitation_methods_ = quantitation_methods;
+  }
+
+  std::vector<OpenMS::AbsoluteQuantitationMethod>& SequenceSegmentHandler::getQuantitationMethods()
+  {
+    return quantitation_methods_;
+  }
+
+  std::vector<OpenMS::AbsoluteQuantitationMethod> SequenceSegmentHandler::getQuantitationMethods() const
+  {
+    return quantitation_methods_;
+  }
+
+  void SequenceSegmentHandler::setComponentsToConcentrations(
+    const std::map<std::string, std::vector<OpenMS::AbsoluteQuantitationStandards::featureConcentration>> components_to_concentrations
+  )
+  {
+    components_to_concentrations_ = components_to_concentrations;
+  }
+
+  std::map<std::string, std::vector<OpenMS::AbsoluteQuantitationStandards::featureConcentration>>&
+  SequenceSegmentHandler::getComponentsToConcentrations()
+  {
+    return components_to_concentrations_;
+  }
+
+  std::map<std::string, std::vector<OpenMS::AbsoluteQuantitationStandards::featureConcentration>>
+  SequenceSegmentHandler::getComponentsToConcentrations() const
+  {
+    return components_to_concentrations_;
+  }
+}
