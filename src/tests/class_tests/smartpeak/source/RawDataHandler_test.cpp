355065b67b52999650e8f4f1e5cf009a17893b51
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2018-07-24 12:56:31 +0200

    style: use longer name for handler
    test: use const

--- src/tests/class_tests/smartpeak/source/RawDataHandler_test.cpp
+++ src/tests/class_tests/smartpeak/source/RawDataHandler_test.cpp
@@ -26,20 +26,20 @@ BOOST_AUTO_TEST_CASE(destructor)
 
 BOOST_AUTO_TEST_CASE(set_or_get_FeatureMap)
 {
-  RawDataHandler raw;
+  RawDataHandler rawDataHandler;
 
   OpenMS::FeatureMap f1;
   f1.setMetaValue("name", "foo");
 
-  raw.setFeatureMap(f1);
+  rawDataHandler.setFeatureMap(f1);
 
-  OpenMS::FeatureMap f2 = raw.getFeatureMap(); // testing copy getter
+  const OpenMS::FeatureMap f2 = rawDataHandler.getFeatureMap(); // testing copy getter
   BOOST_CHECK_EQUAL(f2.metaValueExists("name"), true);
   BOOST_CHECK_EQUAL(f2.getMetaValue("name"), "foo");
 
-  raw.getFeatureMap().setMetaValue("name2", "bar"); // testing reference getter
+  rawDataHandler.getFeatureMap().setMetaValue("name2", "bar"); // testing reference getter
 
-  OpenMS::FeatureMap& f3 = raw.getFeatureMap();
+  const OpenMS::FeatureMap& f3 = rawDataHandler.getFeatureMap();
   BOOST_CHECK_EQUAL(f3.metaValueExists("name"), true);
   BOOST_CHECK_EQUAL(f3.getMetaValue("name"), "foo");
   BOOST_CHECK_EQUAL(f3.metaValueExists("name2"), true);
@@ -48,21 +48,21 @@ BOOST_AUTO_TEST_CASE(set_or_get_FeatureMap)
 
 BOOST_AUTO_TEST_CASE(set_or_get_MetaData)
 {
-  RawDataHandler raw;
+  RawDataHandler rawDataHandler;
 
   MetaDataHandler m1;
-  string name {"foo"};
+  const string name {"foo"};
   m1.setSampleName(name);
 
-  raw.setMetaData(m1);
+  rawDataHandler.setMetaData(m1);
 
-  MetaDataHandler m2 = raw.getMetaData(); // testing copy getter
+  const MetaDataHandler m2 = rawDataHandler.getMetaData(); // testing copy getter
   BOOST_CHECK_EQUAL(m2.getSampleName(), name);
 
-  string group_name {"bar"};
-  raw.getMetaData().setSampleGroupName(group_name); // testing reference getter
+  const string group_name {"bar"};
+  rawDataHandler.getMetaData().setSampleGroupName(group_name); // testing reference getter
 
-  MetaDataHandler& m3 = raw.getMetaData();
+  const MetaDataHandler& m3 = rawDataHandler.getMetaData();
   BOOST_CHECK_EQUAL(m3.getSampleName(), name);
   BOOST_CHECK_EQUAL(m3.getSampleGroupName(), group_name);
 }
@@ -70,57 +70,57 @@ BOOST_AUTO_TEST_CASE(set_or_get_MetaData)
 BOOST_AUTO_TEST_CASE(set_or_get_Parameters)
 {
   map<string, vector<map<string, string>>> parameters;
-  RawDataHandler raw;
+  RawDataHandler rawDataHandler;
   // TODO: missing tests
   // https://github.com/dmccloskey/SmartPeak2/issues/51#issuecomment-406987883
 }
 
 BOOST_AUTO_TEST_CASE(set_or_get_QuantitationMethods)
 {
-  RawDataHandler raw;
+  RawDataHandler rawDataHandler;
 
   OpenMS::AbsoluteQuantitationMethod aqm;
-  string name {"foo"};
+  const string name {"foo"};
   aqm.setComponentName(name);
 
   vector<OpenMS::AbsoluteQuantitationMethod> AQMs1;
   AQMs1.push_back(aqm);
 
-  raw.setQuantitationMethods(AQMs1);
+  rawDataHandler.setQuantitationMethods(AQMs1);
 
-  vector<OpenMS::AbsoluteQuantitationMethod> AQMs2 = raw.getQuantitationMethods(); // testing copy getter
+  const vector<OpenMS::AbsoluteQuantitationMethod> AQMs2 = rawDataHandler.getQuantitationMethods(); // testing copy getter
   BOOST_CHECK_EQUAL(AQMs2.size(), 1);
   BOOST_CHECK_EQUAL(AQMs2[0].getComponentName(), name);
 
-  string feature_name {"bar"};
-  raw.getQuantitationMethods()[0].setFeatureName(feature_name); // testing reference getter
+  const string feature_name {"bar"};
+  rawDataHandler.getQuantitationMethods()[0].setFeatureName(feature_name); // testing reference getter
 
-  vector<OpenMS::AbsoluteQuantitationMethod>& AQMs3 = raw.getQuantitationMethods();
+  const vector<OpenMS::AbsoluteQuantitationMethod>& AQMs3 = rawDataHandler.getQuantitationMethods();
   BOOST_CHECK_EQUAL(AQMs3[0].getComponentName(), name);
   BOOST_CHECK_EQUAL(AQMs3[0].getFeatureName(), feature_name);
 }
 
 BOOST_AUTO_TEST_CASE(set_or_get_FeatureFilter)
 {
-  RawDataHandler raw;
+  RawDataHandler rawDataHandler;
 
   OpenMS::MRMFeatureQC::ComponentQCs qc;
-  string name {"foo"};
+  const string name {"foo"};
   qc.component_name = name;
 
   OpenMS::MRMFeatureQC fqc1;
   fqc1.component_qcs.push_back(qc);
 
-  raw.setFeatureFilter(fqc1);
+  rawDataHandler.setFeatureFilter(fqc1);
 
-  OpenMS::MRMFeatureQC fqc2 = raw.getFeatureFilter(); // testing copy getter
+  const OpenMS::MRMFeatureQC fqc2 = rawDataHandler.getFeatureFilter(); // testing copy getter
   BOOST_CHECK_EQUAL(fqc2.component_qcs.size(), 1);
   BOOST_CHECK_EQUAL(fqc2.component_qcs[0].component_name, name);
 
   const double rt_low {4.0};
-  raw.getFeatureFilter().component_qcs[0].retention_time_l = rt_low; // testing reference getter
+  rawDataHandler.getFeatureFilter().component_qcs[0].retention_time_l = rt_low; // testing reference getter
 
-  OpenMS::MRMFeatureQC& fqc3 = raw.getFeatureFilter();
+  const OpenMS::MRMFeatureQC& fqc3 = rawDataHandler.getFeatureFilter();
   BOOST_CHECK_EQUAL(fqc3.component_qcs.size(), 1);
   BOOST_CHECK_EQUAL(fqc3.component_qcs[0].component_name, name);
   BOOST_CHECK_EQUAL(fqc3.component_qcs[0].retention_time_l, rt_low);
@@ -128,25 +128,25 @@ BOOST_AUTO_TEST_CASE(set_or_get_FeatureFilter)
 
 BOOST_AUTO_TEST_CASE(set_or_get_FeatureQC)
 {
-  RawDataHandler raw;
+  RawDataHandler rawDataHandler;
 
   OpenMS::MRMFeatureQC::ComponentQCs qc;
-  string name {"foo"};
+  const string name {"foo"};
   qc.component_name = name;
 
   OpenMS::MRMFeatureQC fqc1;
   fqc1.component_qcs.push_back(qc);
 
-  raw.setFeatureQC(fqc1);
+  rawDataHandler.setFeatureQC(fqc1);
 
-  OpenMS::MRMFeatureQC fqc2 = raw.getFeatureQC(); // testing copy getter
+  const OpenMS::MRMFeatureQC fqc2 = rawDataHandler.getFeatureQC(); // testing copy getter
   BOOST_CHECK_EQUAL(fqc2.component_qcs.size(), 1);
   BOOST_CHECK_EQUAL(fqc2.component_qcs[0].component_name, name);
 
   const double rt_low {4.0};
-  raw.getFeatureQC().component_qcs[0].retention_time_l = rt_low; // testing reference getter
+  rawDataHandler.getFeatureQC().component_qcs[0].retention_time_l = rt_low; // testing reference getter
 
-  OpenMS::MRMFeatureQC& fqc3 = raw.getFeatureQC();
+  const OpenMS::MRMFeatureQC& fqc3 = rawDataHandler.getFeatureQC();
   BOOST_CHECK_EQUAL(fqc3.component_qcs.size(), 1);
   BOOST_CHECK_EQUAL(fqc3.component_qcs[0].component_name, name);
   BOOST_CHECK_EQUAL(fqc3.component_qcs[0].retention_time_l, rt_low);
@@ -154,23 +154,23 @@ BOOST_AUTO_TEST_CASE(set_or_get_FeatureQC)
 
 BOOST_AUTO_TEST_CASE(set_or_get_FeatureMapHistory)
 {
-  RawDataHandler raw;
+  RawDataHandler rawDataHandler;
 
   OpenMS::FeatureMap f;
   f.setMetaValue("name", "foo");
   vector<OpenMS::FeatureMap> f1;
   f1.push_back(f);
 
-  raw.setFeatureMapHistory(f1);
+  rawDataHandler.setFeatureMapHistory(f1);
 
-  vector<OpenMS::FeatureMap> f2 = raw.getFeatureMapHistory(); // testing copy getter
+  const vector<OpenMS::FeatureMap> f2 = rawDataHandler.getFeatureMapHistory(); // testing copy getter
   BOOST_CHECK_EQUAL(f2.size(), 1);
   BOOST_CHECK_EQUAL(f2[0].metaValueExists("name"), true);
   BOOST_CHECK_EQUAL(f2[0].getMetaValue("name"), "foo");
 
-  raw.getFeatureMapHistory()[0].setMetaValue("name2", "bar"); // testing reference getter
+  rawDataHandler.getFeatureMapHistory()[0].setMetaValue("name2", "bar"); // testing reference getter
 
-  vector<OpenMS::FeatureMap>& f3 = raw.getFeatureMapHistory();
+  const vector<OpenMS::FeatureMap>& f3 = rawDataHandler.getFeatureMapHistory();
   BOOST_CHECK_EQUAL(f3.size(), 1);
   BOOST_CHECK_EQUAL(f3[0].metaValueExists("name"), true);
   BOOST_CHECK_EQUAL(f3[0].getMetaValue("name"), "foo");
