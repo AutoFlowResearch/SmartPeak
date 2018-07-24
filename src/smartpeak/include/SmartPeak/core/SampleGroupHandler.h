ee0ca920e1f6288e971c83f643f4a79a105d9375
Pasquale Colaianni <pasqoo@users.noreply.github.com>
2018-07-24 12:31:21 +0200

    [TEST] SampleGroupHandler (#53)
    * style: fix indentation in cpp file
    
    * fix: use const in args
    
    test: implement tests
    
    * test: use const

--- src/smartpeak/include/SmartPeak/core/SampleGroupHandler.h
+++ src/smartpeak/include/SmartPeak/core/SampleGroupHandler.h
@@ -13,11 +13,11 @@ public:
     SampleGroupHandler() = default;
     ~SampleGroupHandler() = default;
 
-    void setSampleGroupName(std::string& sample_group_name);
+    void setSampleGroupName(const std::string& sample_group_name);
     std::string& getSampleGroupName();
     std::string getSampleGroupName() const;
 
-    void setSampleIndices(std::vector<size_t>& sample_indices);
+    void setSampleIndices(const std::vector<size_t>& sample_indices);
     std::vector<size_t>& getSampleIndices();
     std::vector<size_t> getSampleIndices() const;
 
