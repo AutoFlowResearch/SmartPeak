c560aa89f23ad4dbf68e7400b328ebe947b990ee
Pasquale Colaianni <pasqoo@users.noreply.github.com>
2018-07-25 10:15:12 +0200

    [TEST] SampleHandler (#55)
    * style: remove unnecessary indent
    
    * test: all tests

--- src/smartpeak/source/core/SampleHandler.cpp
+++ src/smartpeak/source/core/SampleHandler.cpp
@@ -4,39 +4,39 @@
 
 namespace SmartPeak
 {
-    void SampleHandler::clear()
-    {
-      meta_data_.clear();
-      // raw_data_.clear(); // TODO: implement .clear() in RawDataHandler
-    }
+  void SampleHandler::clear()
+  {
+    meta_data_.clear();
+    // raw_data_.clear(); // TODO: implement .clear() in RawDataHandler
+  }
 
-    void SampleHandler::setMetaData(const MetaDataHandler& meta_data)
-    {
-      meta_data_ = meta_data;
-    }
+  void SampleHandler::setMetaData(const MetaDataHandler& meta_data)
+  {
+    meta_data_ = meta_data;
+  }
 
-    MetaDataHandler& SampleHandler::getMetaData()
-    {
-      return meta_data_;
-    }
+  MetaDataHandler& SampleHandler::getMetaData()
+  {
+    return meta_data_;
+  }
 
-    MetaDataHandler SampleHandler::getMetaData() const
-    {
-      return meta_data_;
-    }
+  MetaDataHandler SampleHandler::getMetaData() const
+  {
+    return meta_data_;
+  }
 
-    void SampleHandler::setRawData(const RawDataHandler& raw_data)
-    {
-      raw_data_ = raw_data;
-    }
+  void SampleHandler::setRawData(const RawDataHandler& raw_data)
+  {
+    raw_data_ = raw_data;
+  }
 
-    RawDataHandler& SampleHandler::getRawData()
-    {
-      return raw_data_;
-    }
+  RawDataHandler& SampleHandler::getRawData()
+  {
+    return raw_data_;
+  }
 
-    RawDataHandler SampleHandler::getRawData() const
-    {
-      return raw_data_;
-    }
+  RawDataHandler SampleHandler::getRawData() const
+  {
+    return raw_data_;
+  }
 }
