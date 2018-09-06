293431975e98ea149ac32b05bbd5bddbb2f17ee7
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2018-09-06 11:24:11 +0200

    test: loadQuantitationMethods

--- src/tests/class_tests/smartpeak/source/OpenMSFile_test.cpp
+++ src/tests/class_tests/smartpeak/source/OpenMSFile_test.cpp
@@ -45,14 +45,40 @@ BOOST_AUTO_TEST_CASE(loadStandardsConcentrations)
   BOOST_CHECK_CLOSE(rc[7].dilution_factor, 1.0, 1e-6);
 }
 
-// BOOST_AUTO_TEST_CASE(loadStandardsConcentrations)
-// {
-//   const string pathname = SMARTPEAK_GET_TEST_DATA_PATH("FileReader_parameters.csv");
-//   SequenceSegmentHandler ssh;
-//   OpenMSFile::loadStandardsConcentrations(ssh, pathname);
-//   const std::vector<OpenMS::AbsoluteQuantitationStandards::runConcentration>& rc = ssh.getStandardsConcentrations();
+BOOST_AUTO_TEST_CASE(loadQuantitationMethods)
+{
+  const string pathname = SMARTPEAK_GET_TEST_DATA_PATH("OpenMSFile_quantitationMethods_1.csv");
+  SequenceSegmentHandler ssh;
+  OpenMSFile::loadQuantitationMethods(ssh, pathname);
+  const std::vector<OpenMS::AbsoluteQuantitationMethod>& aqm = ssh.getQuantitationMethods();
 
-//   BOOST_CHECK_EQUAL(rc.size(), 8);
-// }
+  BOOST_CHECK_EQUAL(aqm.size(), 107);
+
+  BOOST_CHECK_EQUAL(aqm[0].getComponentName(), "23dpg.23dpg_1.Light");
+  BOOST_CHECK_EQUAL(aqm[0].getFeatureName(), "peak_apex_int");
+  BOOST_CHECK_EQUAL(aqm[0].getISName(), "23dpg.23dpg_1.Heavy");
+  BOOST_CHECK_EQUAL(aqm[0].getConcentrationUnits(), "uM");
+  BOOST_CHECK_EQUAL(aqm[0].getTransformationModel(), "linear");
+  BOOST_CHECK_CLOSE(aqm[0].getLLOD(), 0.0, 1e-6);
+  BOOST_CHECK_CLOSE(aqm[0].getULOD(), 0.0, 1e-6);
+  BOOST_CHECK_CLOSE(aqm[0].getLLOQ(), 0.25, 1e-6);
+  BOOST_CHECK_CLOSE(aqm[0].getULOQ(), 2.5, 1e-6);
+  BOOST_CHECK_CLOSE(aqm[0].getCorrelationCoefficient(), 0.983846949, 1e-6);
+  BOOST_CHECK_EQUAL(aqm[0].getNPoints(), 4);
+
+  BOOST_CHECK_EQUAL(aqm[106].getComponentName(), "xan.xan_1.Light");
+  BOOST_CHECK_EQUAL(aqm[106].getFeatureName(), "peak_apex_int");
+  BOOST_CHECK_EQUAL(aqm[106].getISName(), "xan.xan_1.Heavy");
+  BOOST_CHECK_EQUAL(aqm[106].getConcentrationUnits(), "uM");
+  BOOST_CHECK_EQUAL(aqm[106].getTransformationModel(), "linear");
+  BOOST_CHECK_CLOSE(aqm[106].getLLOD(), 0.0, 1e-6);
+  BOOST_CHECK_CLOSE(aqm[106].getULOD(), 0.0, 1e-6);
+  BOOST_CHECK_CLOSE(aqm[106].getLLOQ(), 0.004, 1e-6);
+  BOOST_CHECK_CLOSE(aqm[106].getULOQ(), 0.16, 1e-6);
+  BOOST_CHECK_CLOSE(aqm[106].getCorrelationCoefficient(), 0.994348761, 1e-6);
+  BOOST_CHECK_EQUAL(aqm[106].getNPoints(), 6);
+
+  // TODO: test Params ?
+}
 
 BOOST_AUTO_TEST_SUITE_END()
