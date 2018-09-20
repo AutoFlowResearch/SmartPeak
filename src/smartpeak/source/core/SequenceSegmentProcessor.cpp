ebb9b17b1ffb9c963fc86496f12b6426544c7a07
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2018-09-20 12:31:55 +0200

    feat: processSequenceSegment()

--- src/smartpeak/source/core/SequenceSegmentProcessor.cpp
+++ src/smartpeak/source/core/SequenceSegmentProcessor.cpp
@@ -95,33 +95,6 @@ namespace SmartPeak
     sequenceSegmentHandler_IO.setQuantitationMethods(absoluteQuantitation.getQuantitationMethods);
   }
 
-  void SequenceSegmentProcessor::calculateVariance(
-    SequenceSegmentHandler& sequenceSegmentHandler_IO,
-    const SequenceHandler& sequenceHandler_I,
-    const std::vector<std::map<std::string, std::string>>& _params_I,
-    const bool verbose_I
-  )
-  {
-    if (verbose_I)
-      std::cout << "Calculating QCs." << std::endl;
-
-    const std::vector<size_t> QC_indices;
-
-    getSampleIndicesBySampleType(
-      sequenceSegmentHandler_IO,
-      sequenceHandler_I, MetaDataHandler::SampleType::QC,
-      QC_indices
-    );
-
-    if (QC_indices.empty())
-      return; // TODO[3]: or throw? print something?
-
-    std::vector<OpenMS::FeatureMap> QC_featureMaps;
-    for (const size_t index : QC_indices) {
-      QC_featureMaps.push_back(sequenceHandler_I.getSequence().at(index).getRawData().getFeatureMap());
-    }
-  }
-
   void SequenceSegmentProcessor::plotCalibrators(
     const SequenceSegmentHandler& sequenceSegmentHandler_I,
     const std::string& calibrators_pdf_o,
@@ -138,5 +111,45 @@ namespace SmartPeak
     SequenceSegmentPlotter sequenceSegmentPlotter;
     sequenceSegmentPlotter.setParameters(SequenceSegmentPlotter_params_I);
     sequenceSegmentPlotter.plotCalibrationPoints(calibrators_pdf_o, sequenceSegmentHandler_I);
+  }
+
+  void SequenceSegmentProcessor::processSequenceSegment(
+    SequenceSegmentHandler& sequenceSegmentHandler_IO,
+    const SequenceHandler& sequenceHandler_I,
+    const std::string& sequence_segment_processing_event,
+    const std::vector<std::map<std::string, std::string>>& parameters,
+    const std::map<std::string, std::string>& filenames,
+    const bool verbose_I
+  )
+  {
+    try {
+      if (sequence_segment_processing_event == "calculate_calibration") {
+        optimizeCalibrationCurves(
+          sequenceSegmentHandler_IO,
+          sequenceHandler_I,
+          parameters.at("AbsoluteQuantitation"),
+          verbose_I
+        );
+        for (const size_t index : sequenceSegmentHandler_IO.getSampleIndices()) {
+          sequenceHandler_I.getSequence().at(index).getRawData()
+            .setQuantitationMethods(sequenceSegmentHandler_IO.getQuantitationMethods());
+        }
+      } else if (sequence_segment_processing_event == "store_quantitation_methods") {
+        OpenMSFile::storeQuantitationMethods(sequenceSegmentHandler_IO, filenames.at("quantitationMethods_csv_o"), verbose_I);
+      } else if (sequence_segment_processing_event == "load_quantitation_methods") {
+        OpenMSFile::loadQuantitationMethods(sequenceSegmentHandler_IO, filenames.at("quantitationMethods_csv_i"), verbose_I);
+      } else if (sequence_segment_processing_event == "plot_calibrators") {
+        plotCalibrators(
+          sequenceSegmentHandler_IO,
+          filenames.at("calibrators_pdf_o"),
+          parameters.at("SequenceSegmentPlotter"),
+          verbose_I
+        );
+      } else {
+        std::cout << "Sequence group processing event " << sequence_segment_processing_event << " was not recognized." << std::endl;
+      }
+    } catch (const std::exception& e) {
+      std::cerr << e.what();
+    }
   }
 }
