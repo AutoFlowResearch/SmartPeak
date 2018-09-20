45d9f1958662bbd148e384447c79c52f8edf000a
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2018-09-20 15:08:37 +0200

    test: getSampleIndicesBySampleType

--- src/tests/class_tests/smartpeak/source/SequenceSegmentProcessor_test.cpp
+++ src/tests/class_tests/smartpeak/source/SequenceSegmentProcessor_test.cpp
@@ -57,4 +57,52 @@ BOOST_AUTO_TEST_CASE(getDefaultSequenceSegmentProcessingWorkflow)
   BOOST_CHECK_EQUAL(workflow[0], "calculate_carryover");
 }
 
+BOOST_AUTO_TEST_CASE(getSampleIndicesBySampleType)
+{
+  MetaDataHandler meta_data1;
+  meta_data1.setFilename("file1");
+  meta_data1.setSampleName("sample1");
+  meta_data1.setSampleGroupName("sample");
+  meta_data1.setSequenceSegmentName("sequence_segment");
+  meta_data1.setSampleType(MetaDataHandler::SampleType::Unknown);
+  OpenMS::FeatureMap featuremap1;
+
+  MetaDataHandler meta_data2;
+  meta_data2.setFilename("file2");
+  meta_data2.setSampleName("sample2");
+  meta_data2.setSampleGroupName("sample");
+  meta_data2.setSequenceSegmentName("sequence_segment");
+  meta_data2.setSampleType(MetaDataHandler::SampleType::Standard);
+  OpenMS::FeatureMap featuremap2;
+
+  MetaDataHandler meta_data3;
+  meta_data3.setFilename("file3");
+  meta_data3.setSampleName("sample3");
+  meta_data3.setSampleGroupName("sample");
+  meta_data3.setSequenceSegmentName("sequence_segment");
+  meta_data3.setSampleType(MetaDataHandler::SampleType::Unknown);
+  OpenMS::FeatureMap featuremap3;
+
+  SequenceHandler sequenceHandler;
+  sequenceHandler.addSampleToSequence(meta_data1, featuremap1);
+  sequenceHandler.addSampleToSequence(meta_data2, featuremap2);
+  sequenceHandler.addSampleToSequence(meta_data3, featuremap3);
+
+  SequenceSegmentHandler sequenceSegmentHandler;
+
+  sequenceSegmentHandler.setSampleIndices({0, 1, 2});
+
+  std::vector<size_t> sample_indices;
+  SequenceSegmentProcessor::getSampleIndicesBySampleType(
+    sequenceSegmentHandler,
+    sequenceHandler,
+    MetaDataHandler::SampleType::Unknown,
+    sample_indices
+  );
+
+  BOOST_CHECK_EQUAL(sample_indices.size(), 2);
+  BOOST_CHECK_EQUAL(sample_indices[0], 0);
+  BOOST_CHECK_EQUAL(sample_indices[1], 2);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
