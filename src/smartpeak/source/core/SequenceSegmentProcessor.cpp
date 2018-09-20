3d2ccab28e0cf74c7200c0512855a0e9aaf592e1
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2018-09-20 09:37:06 +0200

    feat: getSampleIndicesBySampleType()

--- src/smartpeak/source/core/SequenceSegmentProcessor.cpp
+++ src/smartpeak/source/core/SequenceSegmentProcessor.cpp
@@ -1,4 +1,19 @@
 namespace SmartPeak
 {
+  SequenceSegmentProcessor::getSampleIndicesBySampleType(
+    const SequenceSegmentHandler& sequenceSegmentHandler,
+    const SequenceHandler& sequenceHandler,
+    const MetaDataHandler::SampleType sampleType,
+    std::vector<size_t>& sampleIndices
+  )
+  {
+    sampleIndices.clear();
+    for (const size_t index : sequenceSegmentHandler.getSampleIndices()) {
+      if (sequenceHandler.getSequenze().at(index).getMetaData().getSampleType() == sampleType) {
+        sampleIndices.push_back(index);
+      }
+    }
+  }
+
 
 }
