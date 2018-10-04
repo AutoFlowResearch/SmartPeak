ca4391a38592a5c822223a8234202e419e6c441c
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2018-10-04 15:19:00 +0200

    chore: remove commented-out code

--- src/smartpeak/source/core/MRMFeatureValidator.cpp
+++ src/smartpeak/source/core/MRMFeatureValidator.cpp
@@ -35,24 +35,12 @@ namespace SmartPeak
         const std::string reference_data_key = subordinate.getMetaValue("native_id").toString();
         OpenMS::Feature subordinate_copy = subordinate;
 
-
-        // const std::vector<std::map<std::string, Utilities::CastValue>>::const_iterator it =
-        //   std::find_if(reference_data_v.cbegin(), reference_data_v.cend(),
-        //     [&reference_data_key](const std::map<std::string, Utilities::CastValue>& row) {return row.at("component_name").s_ == reference_data_key;}
-        //   );
-        // if (it == reference_data_v.cend()) {
-        //   subordinate_copy.setMetaValue("validation", "ND");
-        //   subordinates_tmp.push_back(subordinate_copy);
-        //   continue;
-        // }
-        // const float reference_rt = it->at("retention_time").f_;
-
-
         if (0 == reference_data.count(reference_data_key)) {
           subordinate_copy.setMetaValue("validation", "ND");
           subordinates_tmp.push_back(subordinate_copy);
           continue;
         }
+
         const float reference_rt = reference_data[reference_data_key]["retention_time"].f_;
 
         if (0.0 == reference_rt) {
