f46a1d4f1101da1d904521614c3a214b151b1371
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2018-11-19 10:19:39 +0100

    chore: cmake config, review comments

--- src/examples/executables.cmake
+++ src/examples/executables.cmake
@@ -1,20 +1,3 @@
-set(ml_executables_list
-  MNIST_AAE_example
-  MNIST_AAE_LabelStyle_example
-  MNIST_CovNet_example
-  MNIST_EvoNet_example
-  MNIST_GAN_example
-  MNIST_LSTM_example
-  MNIST_VAE_example
-  AdditionProblem_example
-  MemoryCell_example
-  Metabolomics_example
-)
-
-set(cuda_executables_list
-  CUDA_example
-)
-
 set(pipeline_executables_list
   LCMS_MRM_Standards_example
   LCMS_MRM_Unknown_example
@@ -23,7 +6,5 @@ set(pipeline_executables_list
 
 ### collect example executables
 set(EXAMPLE_executables
-#  ${ml_executables_list}
-#  ${cuda_executables_list}
   ${pipeline_executables_list}
 )
