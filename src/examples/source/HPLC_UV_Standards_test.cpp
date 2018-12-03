1c07d84d09da8ff163a1245ccf73fb3f669bc3f0
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2018-12-03 12:46:33 +0100

    test: remove couts

--- src/examples/source/HPLC_UV_Standards_test.cpp
+++ src/examples/source/HPLC_UV_Standards_test.cpp
@@ -31,14 +31,9 @@ void test_main_HPLC_UV_Standards()
 
   OpenMS::FeatureMap fm2 = rawDataHandler.getFeatureMap();
 
-cout << "fm1 size: " << fm1.size() << endl;
-cout << "fm2 size: " << fm2.size() << endl;
-
   assert(fm1.size() == 8);
   assert(fm1.size() == fm2.size());
 
-cout << "fm1[0].getSubordinates().size(): " << fm1[0].getSubordinates().size() << endl;
-cout << "fm1[0].getSubordinates().size(): " << fm2[0].getSubordinates().size() << endl;
   assert(fm1[0].getSubordinates().size() == 2);
   assert(fm1[0].getSubordinates().size() == fm2[0].getSubordinates().size());
   const OpenMS::Feature* f1 = &fm1[0].getSubordinates()[0];
@@ -47,8 +42,6 @@ cout << "fm1[0].getSubordinates().size(): " << fm2[0].getSubordinates().size() <
   assert(Utilities::assert_close((double)f1->getMetaValue("peak_apex_int"), (double)f2->getMetaValue("peak_apex_int")));
   assert(Utilities::assert_close((double)f1->getRT(), (double)f2->getRT()));
 
-cout << "fm1[6].getSubordinates().size(): " << fm1[6].getSubordinates().size() << endl;
-cout << "fm1[6].getSubordinates().size(): " << fm2[6].getSubordinates().size() << endl;
   assert(fm1[6].getSubordinates().size() == 2);
   assert(fm1[6].getSubordinates().size() == fm2[6].getSubordinates().size());
   f1 = &fm1[6].getSubordinates()[0];
