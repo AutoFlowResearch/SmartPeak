092a1000fb9c780df66bc6c05253c0e95a30290b
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2018-12-04 13:21:21 +0100

    test: LCMS_MRM_Unknowns

--- src/examples/source/LCMS_MRM_Unknown_test.cpp
+++ src/examples/source/LCMS_MRM_Unknown_test.cpp
@@ -31,15 +31,51 @@ void test_main_LCMS_MRM_Unknown()
 
   OpenMS::FeatureMap fm2 = rawDataHandler.getFeatureMap();
 
-  assert(fm1.size() == 0); // TODO: 0 is supposed to fail
+cout << "fm1 size: " << fm1.size() << endl;
+cout << "fm2 size: " << fm2.size() << endl;
+  assert(fm1.size() == 92);
   assert(fm1.size() == fm2.size());
 
-  const OpenMS::Feature& f1 = fm1[50].getSubordinates()[0];
-  const OpenMS::Feature& f2 = fm2[50].getSubordinates()[0];
+cout << "fm1[0].getSubordinates().size(): " << fm1[0].getSubordinates().size() << endl;
+cout << "fm2[0].getSubordinates().size(): " << fm2[0].getSubordinates().size() << endl;
+  assert(fm1[0].getSubordinates().size() == 2);
+  assert(fm1[0].getSubordinates().size() == fm2[0].getSubordinates().size());
 
-  assert(f1.getMetaValue("native_id") == f2.getMetaValue("native_id"));
-  assert(Utilities::assert_close((double)f1.getMetaValue("peak_apex_int"), (double)f2.getMetaValue("peak_apex_int")));
-  assert(Utilities::assert_close((double)f1.getRT(), (double)f2.getRT()));
+  const OpenMS::Feature* f1 = &fm1[0].getSubordinates()[0];
+  const OpenMS::Feature* f2 = &fm2[0].getSubordinates()[0];
+cout << "0 0 native_id: " << f1->getMetaValue("native_id") << endl;
+cout << "0 0 peak_apex_int: " << f1->getMetaValue("peak_apex_int") << endl;
+cout << "0 0 getRT: " << f1->getRT() << endl;
+  assert(f1->getMetaValue("native_id") == "23dpg.23dpg_1.Heavy");
+  assert(Utilities::assert_close((double)f1->getMetaValue("peak_apex_int"), 249.465663036488));
+  assert(Utilities::assert_close((double)f1->getRT(), 945.205));
+  assert(f1->getMetaValue("native_id") == f2->getMetaValue("native_id"));
+  assert(Utilities::assert_close((double)f1->getMetaValue("peak_apex_int"), (double)f2->getMetaValue("peak_apex_int")));
+  assert(Utilities::assert_close((double)f1->getRT(), (double)f2->getRT()));
+
+  f1 = &fm1[6].getSubordinates()[0];
+  f2 = &fm2[6].getSubordinates()[0];
+cout << "6 0 native_id: " << f1->getMetaValue("native_id") << endl;
+cout << "6 0 peak_apex_int: " << f1->getMetaValue("peak_apex_int") << endl;
+cout << "6 0 getRT: " << f1->getRT() << endl;
+  assert(f1->getMetaValue("native_id") == "actp.actp_1.Heavy");
+  assert(Utilities::assert_close((double)f1->getMetaValue("peak_apex_int"), 228.975008430023));
+  assert(Utilities::assert_close((double)f1->getRT(), 745.612));
+  assert(f1->getMetaValue("native_id") == f2->getMetaValue("native_id"));
+  assert(Utilities::assert_close((double)f1->getMetaValue("peak_apex_int"), (double)f2->getMetaValue("peak_apex_int")));
+  assert(Utilities::assert_close((double)f1->getRT(), (double)f2->getRT()));
+
+  f1 = &fm1[50].getSubordinates()[0];
+  f2 = &fm2[50].getSubordinates()[0];
+cout << "50 0 native_id: " << f1->getMetaValue("native_id") << endl;
+cout << "50 0 peak_apex_int: " << f1->getMetaValue("peak_apex_int") << endl;
+cout << "50 0 getRT: " << f1->getRT() << endl;
+  assert(f1->getMetaValue("native_id") == "gsn.gsn_1.Heavy");
+  assert(Utilities::assert_close((double)f1->getMetaValue("peak_apex_int"), 65212.2196216507));
+  assert(Utilities::assert_close((double)f1->getRT(), 84.7791));
+  assert(f1->getMetaValue("native_id") == f2->getMetaValue("native_id"));
+  assert(Utilities::assert_close((double)f1->getMetaValue("peak_apex_int"), (double)f2->getMetaValue("peak_apex_int")));
+  assert(Utilities::assert_close((double)f1->getRT(), (double)f2->getRT()));
 }
 
 int main(int argc, char **argv)
