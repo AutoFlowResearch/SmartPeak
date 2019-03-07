2ed9ca91af18a8b559ec1b20b4f0f0878d3cac73
Eren Yagdiran <ereyag@nnfcb-l0571.clients.wireless.dtu.dk>
2019-03-07 12:08:51 +0100

    fix bat format

--- util/install-dep-windows/install-dependencies-win.bat
+++ util/install-dep-windows/install-dependencies-win.bat
@@ -8,6 +8,6 @@ REM xsd, xerces-c by download
 echo %install%
 
 REM Installing xsd + xerces-c
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\xsd.ps1' -dir %install% -src 'http://www.codesynthesis.com/download/xsd/4.0/windows/i686/xsd-4.0.msi' -Verbose"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\xsd.ps1' -dir 'C:\projects\OpenMS-build\xsd-4.0.msi' -src 'http://www.codesynthesis.com/download/xsd/4.0/windows/i686/xsd-4.0.msi' -Verbose"
 
 
