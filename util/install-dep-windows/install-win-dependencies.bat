c1516651780c887ba867acbc4ecf49d8055737bc
Eren Yagdiran <ereyag@nnfcb-l0571.clients.wireless.dtu.dk>
2019-03-07 10:30:03 +0100

    test xerces

---
+++ util/install-dep-windows/install-win-dependencies.bat
@@ -0,0 +1,13 @@
+
+@echo on
+
+set install="C:\projects\OpenMS-build"
+
+REM xsd, xerces-c by download
+
+echo %install%
+
+REM Installing xsd + xerces-c
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\xsd.ps1' -dir %install% -src 'http://www.codesynthesis.com/download/xsd/4.0/windows/i686/xsd-4.0.msi' -Verbose"
+
+
