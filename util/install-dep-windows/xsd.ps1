39738c714b4a347624b205ae4270e4891332682a
Eren Yagdiran <ereyag@nnfcb-l0571.clients.wireless.dtu.dk>
2019-03-07 12:12:55 +0100

    change write

--- util/install-dep-windows/xsd.ps1
+++ util/install-dep-windows/xsd.ps1
@@ -7,20 +7,20 @@ $install_path = $dir+"\"
 $xsd_msi = $install_path+'xsd-4.0.msi'
 $xsd_dl = $src.AbsoluteUri
 
-Write-Verbose $xsd_dl
-Write-Verbose $xsd_msi
+Write-Host $xsd_dl
+Write-Host $xsd_msi
 
 if((Test-Path $xsd_msi) -eq $false) {
-    Write-Verbose "Downloading xsd package"
-    Write-Verbose $xsd_dl
-    Write-Verbose $xsd_msi
+    Write-Host "Downloading xsd package"
+    Write-Host $xsd_dl
+    Write-Host $xsd_msi
     (new-object System.Net.WebClient).DownloadFile($xsd_dl,$xsd_msi)
 }
 
 $xsd_i = $install_path+"xsd"
 if((Test-Path $xsd_i) -eq $false) {
   # Install xsd+xerces-c package
-  Write-Verbose "Installing $xsd_msi"
+  Write-Host "Installing $xsd_msi"
   msiexec /passive BASEDIR=$xsd_i /i $xsd_msi /lp xsd.log
-  Write-Verbose "Done."
+  Write-Host "Done."
 }
