cd02e587206e895a8ffb440cc3b2eaff4d786a78
Eren Yagdiran <ereyag@nnfcb-l0571.clients.wireless.dtu.dk>
2019-03-11 11:36:48 +0100

    fix and repeat

--- util/install-dep-windows/libsvm.ps1
+++ util/install-dep-windows/libsvm.ps1
@@ -1,2 +1,2 @@
 
-(new-object System.Net.WebClient).DownloadFile("http://www.csie.ntu.edu.tw/~cjlin/cgi-bin/libsvm.cgi?+http://www.csie.ntu.edu.tw/~cjlin/libsvm+zip","C:\projects\deps\libsvm.zip")
+(new-object System.Net.WebClient).DownloadFile("https://github.com/cjlin1/libsvm/archive/v291.zip","C:\projects\deps\libsvm.zip")
