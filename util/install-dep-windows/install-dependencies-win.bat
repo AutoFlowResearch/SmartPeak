417a576332addcd6f34513014a6f047d1a7b8d84
Eren Yagdiran <ereyag@nnfcb-l0571.clients.wireless.dtu.dk>
2019-03-12 13:19:44 +0100

    fix and repeat

--- util/install-dep-windows/install-dependencies-win.bat
+++ util/install-dep-windows/install-dependencies-win.bat
@@ -8,8 +8,9 @@ REM xsd, xerces-c by download
 echo %install%
 
 REM Installing xsd + xerces-c
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\xsd.ps1' -dir %install% -src 'http://www.codesynthesis.com/download/xsd/4.0/windows/i686/xsd-4.0.msi' -Verbose"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\seqan.ps1' -Verbose"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\libsvm.ps1' -Verbose"
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\xsd.ps1' -dir %install% -src 'http://www.codesynthesis.com/download/xsd/4.0/windows/i686/xsd-4.0.msi' -Verbose"
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\seqan.ps1' -Verbose"
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\libsvm.ps1' -Verbose"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\contrib.ps1' -Verbose"
 
 
