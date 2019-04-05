364de09791e5784c4409d07c3164e61d31ad8541
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2019-04-05 10:26:01 +0200

    fix: remove double quotes from imgui url

--- external/imgui.cmake
+++ external/imgui.cmake
@@ -3,12 +3,11 @@ include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
 set_property (DIRECTORY PROPERTY EP_BASE Dependencies)
 
 set (DEPENDENCIES)
-
 set (IMGUI_VERSION master)
-message (STATUS "Adding ImGUI ${IMGUI_VERSION} as an external project.")
+message (STATUS "Adding imgui ${IMGUI_VERSION} as an external project.")
 
 ExternalProject_Add(imgui
-  GIT_REPOSITORY "https://github.com/ocornut/imgui.git"
+  GIT_REPOSITORY https://github.com/ocornut/imgui.git
   GIT_TAG ${IMGUI_VERSION}
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
