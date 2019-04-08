a75a22583b4b9150fcbb69267da2705fe53ae5f4
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2019-04-08 10:08:57 +0200

    style: print plog version in cmake message

--- external/plog.cmake
+++ external/plog.cmake
@@ -4,7 +4,7 @@ set_property (DIRECTORY PROPERTY EP_BASE Dependencies)
 
 set (DEPENDENCIES)
 set (PLOG_VERSION 5f69a631086bf038e2c0feee4319a485c34fa863)
-message (STATUS "Adding plog as an external project.")
+message (STATUS "Adding plog ${PLOG_VERSION} as an external project.")
 
 ExternalProject_Add(plog
   GIT_REPOSITORY https://github.com/SergiusTheBest/plog.git
