ea30399d9615d137fe77edd61be273f6b70c624e
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2019-05-06 10:08:38 +0200

    style: ConsoleFormatter hides messages with severity > plog::info

--- src/smartpeak/source/ui/CommandLine.cpp
+++ src/smartpeak/source/ui/CommandLine.cpp
@@ -1224,15 +1224,19 @@ namespace SmartPeak
 
       static plog::util::nstring format(const plog::Record& record)
       {
+        if (record.getSeverity() > plog::info) {
+          return "";
+        }
+
         std::tm t;
         (plog::util::localtime_s)(&t, &record.getTime().time);
 
         plog::util::nostringstream ss;
         ss << t.tm_year + 1900 << "-" << std::setfill(PLOG_NSTR('0')) << std::setw(2) << t.tm_mon + 1 << PLOG_NSTR("-") << std::setfill(PLOG_NSTR('0')) << std::setw(2) << t.tm_mday << PLOG_NSTR(" ");
         ss << std::setfill(PLOG_NSTR('0')) << std::setw(2) << t.tm_hour << PLOG_NSTR(":") << std::setfill(PLOG_NSTR('0')) << std::setw(2) << t.tm_min << PLOG_NSTR(":") << std::setfill(PLOG_NSTR('0')) << std::setw(2) << t.tm_sec << PLOG_NSTR(" ");
-        ss << std::setfill(PLOG_NSTR(' ')) << std::setw(5) << std::left << severityToString(record.getSeverity()) << PLOG_NSTR(" ");
+        // ss << std::setfill(PLOG_NSTR(' ')) << std::setw(5) << std::left << severityToString(record.getSeverity()) << PLOG_NSTR(" ");
         // ss << PLOG_NSTR("[") << record.getTid() << PLOG_NSTR("] ");
-        ss << PLOG_NSTR("\n") << record.getMessage() << PLOG_NSTR("\n");
+        ss << record.getMessage() << PLOG_NSTR("\n");
 
         return ss.str();
       }
