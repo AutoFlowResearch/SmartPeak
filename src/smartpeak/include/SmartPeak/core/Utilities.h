6c9d88305f45ba5fe24c5bbe02c183ea8cc563da
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2019-05-20 12:50:36 +0200

    docs: Add docstrings for functions endsWith, getPathnameContent, getParentPathname, applyPermutation, is_less_than_icase, directorySize

--- src/smartpeak/include/SmartPeak/core/Utilities.h
+++ src/smartpeak/include/SmartPeak/core/Utilities.h
@@ -188,19 +188,61 @@ public:
     // To validate quantitation methods stored in examples' tests
     static bool testStoredQuantitationMethods(const std::string& pathname);
 
+    /**
+      @brief Check if str ends with suffix
+
+      Useful for filtering filenames by the extension
+
+      @param[in] str Input string
+      @param[in] suffix Suffix
+      @param[in] case_sensitive Case sensitive string comparison
+
+      @returns True if str ends with suffix. Otherwise false.
+    */
     static bool endsWith(
       std::string str,
       std::string suffix,
       const bool case_sensitive = true
     );
 
+    /**
+      @brief Get information about name, size, type and modified date of all
+      entries in a folder
+
+      @note Elements "." and ".." are not part of the content
+
+      @param[in] pathname Folder pathname
+      @param[in] asc Sort entries in ascending order
+
+      @returns A container with the desired information
+    */
     static std::array<std::vector<std::string>, 4> getPathnameContent(
       const std::string& pathname,
       const bool asc = true
     );
 
+    /**
+      @brief Get parent pathname from a pathname string
+
+      @param[in] pathname Input string pathname
+
+      @returns A string representation of the parent pathname
+    */
     static std::string getParentPathname(const std::string& pathname);
 
+    /**
+      @brief Moves the elements in vector v according to indices
+
+      Example:
+      indices: {3, 4, 2, 0, 1}
+      old v:   {'a', 'b', 'c', 'd', 'e'}
+      new v:   {'d', 'e', 'c', 'a', 'b'}
+
+      @throws std::invalid_argument Sizes of indices and v differ
+
+      @param[in] indices The vector of indices that decides the permutation
+      @param[in,out] v The vector of elements to be permuted
+    */
     template<typename T>
     static void applyPermutation(
       const std::vector<size_t>& indices,
@@ -216,8 +258,27 @@ public:
       }
     }
 
+    /**
+      @brief Case-insensitive string comparison
+
+      Equivalent to a.compare(b) < 0, ignoring letter case.
+
+      @param[in] a left-side string
+      @param[in] b right-side string
+
+      @returns True if 'a' is lexicographically less than 'b'. Otherwise false.
+    */
     static bool is_less_than_icase(const std::string& a, const std::string& b);
 
+    /**
+      @brief Count the number of elements in a folder
+
+      @note Elements "." and ".." are not counted
+
+      @param[in] pathname Pathname to a folder
+
+      @returns The numbers of elements found
+    */
     static size_t directorySize(const std::string& pathname);
   };
 }
