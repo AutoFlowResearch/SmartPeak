1a41108e85d7d078cd5d44d7819eecc5fe65c379
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2019-06-24 13:55:37 +0200

    feat: GuiAppender is thread-safe
    style: restrict GARecord visibility to class GuiAppender (private)

--- src/smartpeak/include/SmartPeak/ui/GuiAppender.h
+++ src/smartpeak/include/SmartPeak/ui/GuiAppender.h
@@ -1,12 +1,11 @@
 #pragma once
 
+#include <mutex>
 #include <vector>
 #include <plog/Log.h>
 
 namespace SmartPeak
 {
-  typedef std::pair<plog::Severity, plog::util::nstring> GARecord; // GuiAppenderRecord
-
   class GuiAppender : public plog::IAppender
   {
   public:
@@ -15,6 +14,8 @@ namespace SmartPeak
     std::vector<plog::util::nstring> getMessageList(plog::Severity severity) const;
 
   private:
+    typedef std::pair<plog::Severity, plog::util::nstring> GARecord; // GuiAppenderRecord
     std::vector<GARecord> messages;
+    mutable std::mutex messages_mutex;
   };
 }
