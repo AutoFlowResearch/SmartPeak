cdb0fbc11c80963d3dbb7f4074a3b7b8d1b78359
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2019-07-08 16:05:08 +0200

    feat: run_examples() expects a pathname to find examples, and supports an optional pathname prefix for grep and tee commands

--- .circleci/run_examples.sh
+++ .circleci/run_examples.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 0 ]
+then
+    echo "Error: please provide the path to the binaries"
+    echo "Example: ./run_example.sh ~/SmartPeak2_build/bin"
+    exit 1
+fi
+
+# workaround for cygwin error in appveyor:
+# https://github.com/appveyor/ci/issues/1956#issuecomment-492008640
+command_prefix=""
+
+if [ $# -eq 2 ]
+then
+    command_prefix=$2
+fi
+
 run_example()
 {
     filename=$1
@@ -16,8 +32,7 @@ run_example()
     return $RET_VALUE
 }
 
-# Assumption: SmartPeak2 build folder
-cd ~/SmartPeak2_build/bin || return 1
+cd $1 || return 1
 
 {
   (run_example GCMS_SIM_Unknown_test)&
@@ -27,7 +42,7 @@ cd ~/SmartPeak2_build/bin || return 1
   (run_example LCMS_MRM_Standards_test)&
   (run_example LCMS_MRM_Unknown_test)&
 } |
-tee examples_log.txt
+${command_prefix}tee examples_log.txt
 
 # When adding a new example, update the comparison n==NUMBER_OF_EXAMPLES
-grep -c "PASS" examples_log.txt | ( read n && (( n==6 )) )
+${command_prefix}grep -c "PASS" examples_log.txt | ( read n && (( n==6 )) )
