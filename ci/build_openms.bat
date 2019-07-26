077d9f64bbcde729d76539bc23493bf587b00cc8
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2019-07-26 14:48:43 +0200

    fix: Disable file-tracking in MSBuild
    Enable: hardlink, clcache-server
    
    fix: clcache does not work with /Zi, changed it to /Z7
    
    Squashed commit of the following:
    
    commit 6eb7baae9d776ef403d53fe12aa4d35a7f81840c
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 14:24:02 2019 +0200
    
        feat:ci: ctest -j2
    
    commit 27ad5ca1ca21a9c1031b170a584f7b2c2349a3a1
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 12:55:18 2019 +0200
    
        fix: Add -PassThru option to Start-Process
    
    commit 48169e79bdcbfa553dc5d70d88205bca44796e8f
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 11:47:17 2019 +0200
    
        ci:feat: use Stop-Process for clcache-server
    
    commit 4adbf136697401b51c36f17206a36f3b6e0f2d85
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 11:24:53 2019 +0200
    
        ci:chore: after_build resets clcache stats
    
        ci:chore: clcache stats are printed before and after the build
    
    commit 62cdc3ff63801ae8cc25d71565f5598f0096b0b6
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 11:16:03 2019 +0200
    
        ci:fix: change the way clcache-server is started
    
        Inspired by bitcoin's appveyor conf
    
    commit 4e336fb4ade9463d1a9ee9f80d4d5390ebede3fe
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 11:00:19 2019 +0200
    
        ci:fix: clcache does not like /Zi
    
        Using /Z7 in Debug build
    
    commit f7b31c40a6beb119effcd9dc2c4aaae4ed7e439c
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 10:54:55 2019 +0200
    
        ci:fix: fix clcache-server name
    
    commit 7d8e1faf0a4a92fabff57ae2a8a941a15f772d72
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 10:49:40 2019 +0200
    
        ci:fix: PATH has correct clcache pathname
    
    commit 3b66a55e1386c229a1d550d8c3bc0cbfe47fec85
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 10:47:17 2019 +0200
    
        ci:feat: Improve clcache performance
    
        Enable hardlink and checksum server
    
    commit 531a03585d92cd8f29cc4c9596ac4ec542177ecb
    Author: Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
    Date:   Fri Jul 26 10:18:10 2019 +0200
    
        ci:fix: disable file-tracking in MSBuild
    
        File-tracking deletes the cache
    
        ci:chore: Print clcache statistics before and after the build

--- ci/build_openms.bat
+++ ci/build_openms.bat
@@ -2,5 +2,5 @@ REM Used in AppVeyor
 mkdir C:\projects\OpenMS-build
 cd C:\projects\OpenMS-build
 cmake -D OPENMS_CONTRIB_LIBS="c:/projects/deps/contrib" -D CMAKE_PREFIX_PATH="c:/Libraries/boost_1_67_0;c:/Qt/5.12.2/msvc2017_64/" -G "Visual Studio 15 2017 Win64" -T host=x64 -D HAS_XSERVER=Off -D ADDRESS_SANITIZER=Off -D WITH_GUI=Off -D ENABLE_TUTORIALS=Off -D GIT_TRACKING=Off -D ENABLE_UPDATE_CHECK=Off -D PYOPENMS=Off -D CMAKE_BUILD_TYPE=Release c:\projects\OpenMS
-msbuild src\openswathalgo\OpenSWATHAlgo.sln /maxcpucount /p:CLToolExe=clcache.exe /p:CLToolPath=C:\projects\clcache\install
-msbuild src\openms\OpenMS.sln /maxcpucount /p:CLToolExe=clcache.exe /p:CLToolPath=C:\projects\clcache\install
+msbuild src\openswathalgo\OpenSWATHAlgo.sln /maxcpucount /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\projects\clcache\install
+msbuild src\openms\OpenMS.sln /maxcpucount /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\projects\clcache\install
