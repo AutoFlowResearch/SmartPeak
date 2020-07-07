49a9e10b42b8ef6702ccbc130d67e36086e8d6a2
Ahmed Khalil <29197392+ahmedskhalil@users.noreply.github.com>
2020-07-07 16:39:38 +0200

    CI : packaging for macOS

--- cmake/package_general.cmake
+++ cmake/package_general.cmake
@@ -23,6 +23,8 @@ set(CPACK_PACKAGE_DESCRIPTION_FILE ${PROJECT_SOURCE_DIR}/cmake/SmartPeakPackageD
 set(CPACK_RESOURCE_FILE_LICENSE ${PROJECT_SOURCE_DIR}/License.txt)
 set(CPACK_RESOURCE_FILE_WELCOME ${PROJECT_SOURCE_DIR}/cmake/SmartPeakPackageResourceWelcomeFile.txt)
 set(CPACK_RESOURCE_FILE_README ${PROJECT_SOURCE_DIR}/cmake/SmartPeakPackageResourceReadme.txt)
+set(BUNDLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/bin/GUI.app")
+set(CPACK_MONOLITHIC_INSTALL on)
 
 # --------------------------------------------------------------------------
 # general components and groupings for SmartPeak installation
@@ -55,11 +57,21 @@ set(CPACK_COMPONENT_DOC_INSTALL_TYPES Full)
 # 4. certain directories must be rediscovered again (i.e., SDL2 and OpenMS)
 find_package(SDL2 REQUIRED)
 if (SDL2_FOUND)
-  # Add the directory for later calls to CPACK
-  string(FIND ${SDL2_LIBRARY} ${VC_LIB_PATH_SUFFIX} SDL2_library_suffix_pos)
-  string(SUBSTRING ${SDL2_LIBRARY} 0 ${SDL2_library_suffix_pos} SDL2_LIBRARY_DIR)
-  set(SDL2_LIBRARIES_DIR "${SDL2_LIBRARY_DIR}${VC_LIB_PATH_SUFFIX}")
+  if (WIN32)
+    # Add the directory for later calls to CPACK
+    string(FIND ${SDL2_LIBRARY} ${VC_LIB_PATH_SUFFIX} SDL2_library_suffix_pos)
+    string(SUBSTRING ${SDL2_LIBRARY} 0 ${SDL2_library_suffix_pos} SDL2_LIBRARY_DIR)
+    set(SDL2_LIBRARIES_DIR "${SDL2_LIBRARY_DIR}${VC_LIB_PATH_SUFFIX}")
+  elseif(APPLE)
+    set(SDL2_LIBRARIES_DIR "")
+	  string(REGEX REPLACE ";.*$" "" SDL2_LIB_DIR "${SDL2_LIBRARY}")
+	  if(EXISTS "${SDL2_LIB_DIR}/Versions/A/Frameworks")
+      set(SDL2_LIBRARIES_DIR "${SDL2_LIB_DIR}/Versions/A")
+      set(HIDAPI_DIR "${SDL2_LIB_DIR}/Versions/A/Frameworks")
+	  endif()
+  endif()
 endif (SDL2_FOUND)
+
 find_package(OpenMS REQUIRED)
 if (OpenMS_FOUND)
   ## Add the directory for later calls to CPACK
@@ -67,20 +79,52 @@ if (OpenMS_FOUND)
   string(FIND ${OpenMS_DIR} "/" OpenMS_DIR_suffix_pos REVERSE)
   string(SUBSTRING ${OpenMS_DIR} 0 ${OpenMS_DIR_suffix_pos} OpenMS_ROOT)
   set(OpenMS_SHARE_DIR ${OpenMS_ROOT}/share/OpenMS)
+  message(">>{OpenMS_ROOT}" ${OpenMS_ROOT})
+  message(">>{OpenMS_DIR}" ${OpenMS_DIR})
 endif (OpenMS_FOUND)
-install(CODE "
- include(InstallRequiredSystemLibraries)
- include(BundleUtilities)
- # GET_BUNDLE_ALL_EXECUTABLES(${CMAKE_RUNTIME_OUTPUT_DIRECTORY} EXECS)
- SET(EXECS ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/Release/GUI.exe)
- SET(DEST \"\${CMAKE_INSTALL_PREFIX}/bin/GUI.exe\")
- execute_process(COMMAND ${CMAKE_COMMAND} -E copy \"\${EXECS}\" \"\${DEST}\")
- SET(DIRS ${Qt5Core_DIR} ${Qt5Network_DIR} ${Qt5_DIR} ${OpenMS_LIBRARY_DIR} ${BOOST_LIBRARYDIR} ${SDL2_LIBRARIES_DIR})
- fixup_bundle(\"\${DEST}\" \"\" \"\${DIRS}\")
+
+if (WIN32)
+  install(CODE "
+  include(InstallRequiredSystemLibraries)
+  include(BundleUtilities)
+  # GET_BUNDLE_ALL_EXECUTABLES(${CMAKE_RUNTIME_OUTPUT_DIRECTORY} EXECS)
+  SET(EXECS ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/Release/GUI.exe)
+  SET(DEST \"\${CMAKE_INSTALL_PREFIX}/bin/GUI.exe\")
+  execute_process(COMMAND ${CMAKE_COMMAND} -E copy \"\${EXECS}\" \"\${DEST}\")
+  SET(DIRS ${Qt5Core_DIR} ${Qt5Network_DIR} ${Qt5_DIR} ${BOOST_LIBRARYDIR} ${SDL2_LIBRARIES_DIR})
+  fixup_bundle(\"\${DEST}\" \"\" \"\${DIRS}\")
+  " 
  " 
- COMPONENT applications)
-install(CODE "
- execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${OpenMS_SHARE_DIR} \"\${CMAKE_INSTALL_PREFIX}/share/OpenMS\")
+  " 
  " 
- COMPONENT share)
+  " 
+  COMPONENT applications)
+  install(CODE "
+  execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${OpenMS_SHARE_DIR} \"\${CMAKE_INSTALL_PREFIX}/share/OpenMS\")
+  " 
+ " 
+  " 
+  COMPONENT share)
+
+ elseif(APPLE)
+
+  install(CODE "
+  set(BU_CHMOD_BUNDLE_ITEMS ON)
+  set(BU_COPY_FULL_FRAMEWORK_CONTENTS ON)
+  include(InstallRequiredSystemLibraries)
+  include(BundleUtilities)
+ 
+  SET(DIRS ${Qt5Core_DIR} ${Qt5Network_DIR} ${Qt5_DIR} ${OpenMS_LIBRARY_DIR} ${BOOST_LIBRARYDIR}  ${SDL2_LIBRARIES_DIR} ${HIDAPI_DIR})
+  fixup_bundle(\"${BUNDLE_OUTPUT_PATH}\" \"\" \"\${DIRS}\")
+  verify_app(\"\${BUNDLE_OUTPUT_PATH}\")
+  " 
+  COMPONENT applications)
+  install(CODE "
+  # execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${OpenMS_SHARE_DIR} \"\${CMAKE_INSTALL_PREFIX}/share/OpenMS\")
+  " 
+ " 
+  " 
+  COMPONENT share)
+
+ endif()
 
