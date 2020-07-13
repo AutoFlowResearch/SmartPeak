8e51e601c0ff5252d42ee8e873e5c5067d4e493b
Ahmed Khalil <29197392+ahmedskhalil@users.noreply.github.com>
2020-07-13 17:17:17 +0200

    preparing for read-the-docs

--- src/smartpeak/conf.py
+++ src/smartpeak/conf.py
@@ -10,42 +10,39 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os, sys
-# sys.path.insert(0, os.path.abspath('.')) 
+import os, subprocess
 
+project     = 'SmartPeak'
+copyright   = '2020, SmartPeak Team'
+author      = 'SmartPeak Team'
 
-# -- Project information -----------------------------------------------------
-
-project = 'SmartPeak'
-copyright = '2020, SmartPeak Team'
-author = 'SmartPeak Team'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [ "breathe"]
+extensions              = [ "breathe"]
 breathe_default_project = "SmartPeak"
 
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+exclude_patterns        = ['_build', 'Thumbs.db', '.DS_Store']
+master_doc              = 'index'
+html_theme              = 'sphinx_rtd_theme'
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
 
-master_doc = 'index'
-# -- Options for HTML output -------------------------------------------------
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+
+docs_build_on_RtD = os.environ.get('READTHEDOCS', None) == 'True'
+
+breathe_projects = {}
+
+if docs_build_on_RtD:
+    input_dir = '../SmartPeak'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['SmartPeak'] = output_dir + '/xml'
+
+
