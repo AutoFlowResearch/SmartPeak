46ab6ed91f4eee94f251946ac5c155ae25d905cd
Ahmed Khalil <29197392+ahmedskhalil@users.noreply.github.com>
2020-07-27 18:13:38 +0200

    macOS packaging: ignoring macOS-specific files

--- src/smartpeak/source/core/Utilities.cpp
+++ src/smartpeak/source/core/Utilities.cpp
@@ -522,7 +522,7 @@ namespace SmartPeak
       }
 
       const std::string filename(entry.path().filename().string());
-      if (filename == "." || filename == "..") {
+      if (filename == "." || filename == ".." || (*(&filename.at(0))) == '.') {
         continue;
       }
 
@@ -595,7 +595,7 @@ namespace SmartPeak
 
     for ( ; it != it_end; it++) {
       const fs::path& filename { it->path().filename() };
-      if (filename == fs::path(".") || filename == fs::path("..")) {
+      if (filename == fs::path(".") || filename == fs::path("..") || filename.generic_string().at(0)  == '.') {
         continue;
       }
       ++n;
