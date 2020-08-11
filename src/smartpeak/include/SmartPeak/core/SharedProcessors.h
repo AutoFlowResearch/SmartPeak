854f39a366a84c24ac9d85ffa9600d86eb0db53d
Ahmed Khalil <29197392+ahmedskhalil@users.noreply.github.com>
2020-08-11 09:09:59 +0200

    Windows CI: building SmartPeak2

--- src/smartpeak/include/SmartPeak/core/SharedProcessors.h
+++ src/smartpeak/include/SmartPeak/core/SharedProcessors.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdlib> // C2039
+#include <string> // C2039
 #include <map>
 #include <memory>
 
@@ -10,3 +11,4 @@ namespace SmartPeak {
   extern const std::map<std::string, std::shared_ptr<RawDataProcessor>> n_to_raw_data_method_;
   extern const std::map<std::string, std::shared_ptr<SequenceSegmentProcessor>> n_to_seq_seg_method_;
 }
+#
