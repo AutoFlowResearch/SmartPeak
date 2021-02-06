a9d7dbfa3a5b782cb514518e85044a0ad7382867
dmccloskey <dmccloskey87@gmail.com>
2021-02-06 19:34:13 +0100

    fix: moved local copy of arguments to before run and join step

--- src/smartpeak/source/core/WorkflowManager.cpp
+++ src/smartpeak/source/core/WorkflowManager.cpp
@@ -36,7 +36,14 @@ namespace SmartPeak {
     }
     application_handler_ = source_app_handler;
     done_ = false;
-    std::thread t(run_and_join, std::ref(application_handler_), std::ref(done_), std::ref(source_app_handler), std::ref(injection_names), std::ref(sequence_segment_names), std::ref(sample_group_names), std::ref(commands));
+
+    // making local copies
+    const std::set<std::string> injection_names_(injection_names);
+    const std::set<std::string> sequence_segment_names_(sequence_segment_names);
+    const std::set<std::string> sample_group_names_(sample_group_names);
+    const std::vector<ApplicationHandler::Command> commands_(commands);
+
+    std::thread t(run_and_join, std::ref(application_handler_), std::ref(done_), std::ref(source_app_handler), injection_names_, sequence_segment_names_, sample_group_names_, commands_);
     LOGD << "Created thread (to be detached): " << t.get_id();
     t.detach();
     LOGD << "Thread has been detached";
@@ -49,19 +56,13 @@ namespace SmartPeak {
 
   void WorkflowManager::run_and_join(ApplicationHandler& application_handler, bool& done, ApplicationHandler& source_app_handler, const std::set<std::string>& injection_names, const std::set<std::string>& sequence_segment_names, const std::set<std::string>& sample_group_names, const std::vector<ApplicationHandler::Command>& commands)
   {
-    //// making local copies
-    //const std::set<std::string> injection_names_(injection_names);
-    //const std::set<std::string> sequence_segment_names_(sequence_segment_names);
-    //const std::set<std::string> sample_group_names_(sample_group_names);
-    //const std::vector<ApplicationHandler::Command> commands_(commands);
-
     // run workflow asynchronously
     std::future<void> f = std::async(
       std::launch::async,
       ApplicationProcessors::processCommands,
       std::ref(application_handler),
-      std::ref(commands),
-      std::ref(injection_names), std::ref(sequence_segment_names), std::ref(sample_group_names)
+      std::cref(commands),
+      std::cref(injection_names), std::cref(sequence_segment_names), std::cref(sample_group_names)
     );
 
     LOGD << "Waiting on async operation...";
