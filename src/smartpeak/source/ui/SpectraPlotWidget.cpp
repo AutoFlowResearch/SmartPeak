db9954f154110f2e2fe68c638a7de994255b3eb7
Bertrand Boudaud <bertrand.boudaud@gmail.com>
2021-02-09 10:35:45 +0100

    [fix/aut816] Fixed chromatogram and spectra range reset when selecting different components.

--- src/smartpeak/source/ui/SpectraPlotWidget.cpp
+++ src/smartpeak/source/ui/SpectraPlotWidget.cpp
@@ -27,21 +27,27 @@ namespace SmartPeak
         component_group_names.insert(selected_transition_groups(i));
     }
 
-    if (refresh_needed_)
+    if ((refresh_needed_) || // data changed
+       ((input_sample_names_ != sample_names) || (input_scan_names_ != scan_names) || (input_component_group_names_ != component_group_names))) // user select different items
     {
       // get the whole graph area
-      current_range_ = std::make_pair(0, 2000);
-      session_handler_.getSpectrumScatterPlot(sequence_handler_, chrom_, current_range_, sample_names, scan_names, component_group_names);
-      current_range_ = slider_min_max_ = input_range_ = std::make_pair(chrom_.x_min_, chrom_.x_max_);
-      refresh_needed_ = false;
-    }
-    if ((input_range_ != current_range_) || (input_scan_names_ != scan_names) || (input_sample_names_ != sample_names) || (input_component_group_names_ != component_group_names))
-    {
-      session_handler_.getSpectrumScatterPlot(sequence_handler_, chrom_, current_range_, sample_names, scan_names,component_group_names);
-      input_range_ = current_range_;
+      session_handler_.getSpectrumScatterPlot(sequence_handler_, chrom_, std::make_pair(0, 2000), sample_names, scan_names, component_group_names);
+      if ((std::abs(slider_min_max_.first - chrom_.x_min_) > std::numeric_limits<double>::epsilon()) ||
+          (std::abs(slider_min_max_.second - chrom_.x_max_) > std::numeric_limits<double>::epsilon()))
+      {
+        // min max changed, reset the sliders and current range
+        current_range_ = slider_min_max_ = std::make_pair(chrom_.x_min_, chrom_.x_max_);
+      }
+      input_range_ = std::make_pair(chrom_.x_min_, chrom_.x_max_);
       input_sample_names_ = sample_names;
       input_scan_names_ = scan_names;
       input_component_group_names_ = component_group_names;
+      refresh_needed_ = false;
+    }
+    else if (input_range_ != current_range_) // user zoom in/out
+    {
+      session_handler_.getSpectrumScatterPlot(sequence_handler_, chrom_, current_range_, sample_names, scan_names,component_group_names);
+      input_range_ = current_range_;
     }
   };
 
