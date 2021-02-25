293380131c26fe5658b37184101a5fa94ca2fc7c
Ahmed Khalil <29197392+ahmedskhalil@users.noreply.github.com>
2021-02-25 12:53:00 +0100

    [LIB] - switching to ImGui v1.80

--- external/imgui.cmake
+++ external/imgui.cmake
@@ -4,7 +4,7 @@ set_property (DIRECTORY PROPERTY EP_BASE Dependencies)
 
 set (DEPENDENCIES)
 #set (IMGUI_VERSION 6a0d0dab5a9f0b9518a2bc9bb456a69895ae0962) # tag: v1.72b, 2019/07/31
-set (IMGUI_VERSION 13f718337ac4df2c66727e85174d48e9a91eabcf) # tables
+set (IMGUI_VERSION v1.80) # tables
 message (STATUS "Adding imgui ${IMGUI_VERSION} as an external project.")
 
 ExternalProject_Add(imgui
