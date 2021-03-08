c4981eb70b993fd61dfa8cd32c8e9a4da5890934
Ahmed Khalil <29197392+ahmedskhalil@users.noreply.github.com>
2021-03-08 22:20:42 +0100

    [TEST] - fixing test

--- src/tests/class_tests/smartpeak/source/Widget_test.cpp
+++ src/tests/class_tests/smartpeak/source/Widget_test.cpp
@@ -140,16 +140,13 @@ BOOST_AUTO_TEST_CASE(GenericTableWidget_sorter)
   
   TestTable1.sorter(Im_table_entries, &sorts_specs, is_scanned);
   
-  std::cout << ">>>Im_table_entries[0].entry_contents[0] : " << Im_table_entries[0].entry_contents[0] << std::endl;
-  std::cout << ">>>Im_table_entries[0].entry_contents[1] : " << Im_table_entries[0].entry_contents[1] << std::endl;
-  
   // post-sorting assertion
   // 1st row
-  //BOOST_CHECK_EQUAL(Im_table_entries[0].entry_contents[0], "xan.xan_1.Light");
-  //BOOST_CHECK_EQUAL(Im_table_entries[0].entry_contents[1], "xan");
+  BOOST_CHECK_EQUAL(Im_table_entries[0].entry_contents[0], "xan.xan_1.Light");
+  BOOST_CHECK_EQUAL(Im_table_entries[0].entry_contents[1], "xan");
   // last row
-  //BOOST_CHECK_EQUAL(Im_table_entries[last_row_idx].entry_contents[0], "2mcit.2mcit_1.Heavy");
-  //BOOST_CHECK_EQUAL(Im_table_entries[last_row_idx].entry_contents[1], "2mcit");
+  BOOST_CHECK_EQUAL(Im_table_entries[last_row_idx].entry_contents[0], "2mcit.2mcit_1.Heavy");
+  BOOST_CHECK_EQUAL(Im_table_entries[last_row_idx].entry_contents[1], "2mcit");
 }
 
 BOOST_AUTO_TEST_CASE(GenericTableWidget_searcher)
