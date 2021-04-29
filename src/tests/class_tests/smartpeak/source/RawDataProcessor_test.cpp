45244ea42462cc1e47d90c0477fea6c91f422b0d
bertrandboudaud <bertrand.boudaud@gmail.com>
2021-04-29 13:18:02 +0200

    Update src/tests/class_tests/smartpeak/source/RawDataProcessor_test.cpp
    Co-authored-by: Douglas McCloskey <dmccloskey@users.noreply.github.com>

--- src/tests/class_tests/smartpeak/source/RawDataProcessor_test.cpp
+++ src/tests/class_tests/smartpeak/source/RawDataProcessor_test.cpp
@@ -1104,7 +1104,7 @@ BOOST_AUTO_TEST_CASE(gettersLoadParameters)
 
 BOOST_AUTO_TEST_CASE(processLoadParameters)
 {
-  // no tests, it calls ParametersPArser::read and LoadParameters::sanitizeRawDataProcessorParameters
+  // no tests, it calls ParametersParser::read and LoadParameters::sanitizeRawDataProcessorParameters
 }
 
 BOOST_AUTO_TEST_CASE(sanitizeRawDataProcessorParameters)
