22d48311f6076fb23a8d172eef870e83a6848211
Ahmed Khalil <29197392+ahmedskhalil@users.noreply.github.com>
2021-10-14 20:11:54 +0200

    typo fix

--- src/examples/executables.cmake
+++ src/examples/executables.cmake
@@ -1,23 +1,23 @@
 set(pipeline_executables_list
-  FIAMS_FullScan_Unknown_test
-  GCMS_FullScan_Unknown_test
-  GCMS_SIM_Unknown_test
-  HPLC_UV_Standards_test
-  HPLC_UV_Unknown_test
-  LCMS_MRM_QCs_test
-  LCMS_MRM_Standards_test
-  LCMS_MRM_Unknown_test
+	FIAMS_FullScan_Unknown_test
+	GCMS_FullScan_Unknown_test
+	GCMS_SIM_Unknown_test
+	HPLC_UV_Standards_test
+	HPLC_UV_Unknown_test
+	LCMS_MRM_QCs_test
+	LCMS_MRM_Standards_test
+	LCMS_MRM_Unknown_test
 )
 
 set(interactive_executables_list
   #CLI // TODO: refactor according to AUT-322
-  SmartPeakCLI
-  SmartPeakGUI
+	SmartPeakCLI
+	SmartPeakGUI
 	SmartPeakServer
 )
 
 ### collect example executables
 set(EXAMPLE_executables
-  ${pipeline_executables_list}
-  ${interactive_executables_list}
+	${pipeline_executables_list}
+	${interactive_executables_list}
 )
