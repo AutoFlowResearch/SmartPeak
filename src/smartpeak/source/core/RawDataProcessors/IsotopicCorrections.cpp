ce6ccbec30569a50e26ea63dc3521c18796f97f7
bertrandboudaud <bertrand.boudaud@gmail.com>
2021-11-24 09:35:29 +0100

    cleared the header inclusions

--- src/smartpeak/source/core/RawDataProcessors/IsotopicCorrections.cpp
+++ src/smartpeak/source/core/RawDataProcessors/IsotopicCorrections.cpp
@@ -26,57 +26,13 @@
 #include <SmartPeak/core/ApplicationHandler.h>
 #include <SmartPeak/core/FeatureFiltersUtils.h>
 
-// load/store raw data
-#include <OpenMS/FORMAT/FileTypes.h>
-#include <OpenMS/FORMAT/ChromeleonFile.h>
-#include <OpenMS/FORMAT/TraMLFile.h>  // load traML as well
-#include <OpenMS/FORMAT/FileHandler.h>
-#include <OpenMS/FORMAT/MzMLFile.h>
-#include <OpenMS/FORMAT/MzTabFile.h>
-#include <OpenMS/ANALYSIS/OPENSWATH/ChromatogramExtractor.h>
-#include <OpenMS/ANALYSIS/OPENSWATH/TargetedSpectraExtractor.h>
 #include <OpenMS/ANALYSIS/QUANTITATION/IsotopeLabelingMDVs.h>
-#include <OpenMS/ANALYSIS/TARGETED/MRMMapping.h>
-#include <OpenMS/KERNEL/SpectrumHelper.h>
 
-#include <OpenMS/FORMAT/MRMFeatureQCFile.h>  // load featureFilter and featureQC
-#include <OpenMS/ANALYSIS/OPENSWATH/TransitionTSVFile.h>  // load traML
-#include <OpenMS/FORMAT/FeatureXMLFile.h>  // load/store featureXML
-#include <SmartPeak/io/InputDataValidation.h> // check filenames and headers
-
-// load validation data and parameters
-#include <SmartPeak/io/ParametersParser.h>
-#ifndef CSV_IO_NO_THREAD
-#define CSV_IO_NO_THREAD
-#endif
-#include <SmartPeak/io/csv.h>
 #include <plog/Log.h>
-
-// feature selection
-#include <OpenMS/ANALYSIS/OPENSWATH/MRMFeatureSelector.h>
-#include <OpenMS/ANALYSIS/OPENSWATH/MRMBatchFeatureSelector.h>
-#include <OpenMS/ANALYSIS/OPENSWATH/PeakIntegrator.h>
-
-#include <SmartPeak/algorithm/MRMFeatureValidator.h>  // feature validaiton
-#include <OpenMS/ANALYSIS/OPENSWATH/MRMFeatureFilter.h>  // feature filter/QC
-#include <OpenMS/ANALYSIS/OPENSWATH/MRMFeatureFinderScoring.h>  // feature picker
-#include <OpenMS/ANALYSIS/QUANTITATION/AbsoluteQuantitation.h> // feature quantification
-#include <OpenMS/MATH/MISC/EmgGradientDescent.h>
-
-// FIA-MS
-#include <OpenMS/ANALYSIS/OPENSWATH/SpectrumAddition.h> // MergeSpectra
-#include <OpenMS/FILTERING/SMOOTHING/SavitzkyGolayFilter.h> // PickMS1Features
-#include <OpenMS/TRANSFORMATIONS/RAW2PEAK/PeakPickerHiRes.h> // PickMS1Features
-#include <OpenMS/FILTERING/NOISEESTIMATION/SignalToNoiseEstimatorMedianRapid.h> // PickMS1Features
-#include <OpenMS/ANALYSIS/ID/AccurateMassSearchEngine.h>
-
-// PickMS2Features
-#include <OpenMS/FILTERING/DATAREDUCTION/MassTraceDetection.h>
-#include <OpenMS/FILTERING/DATAREDUCTION/ElutionPeakDetection.h>
-#include <OpenMS/FILTERING/DATAREDUCTION/FeatureFindingMetabo.h>
 
 #include <algorithm>
 #include <exception>
+
 
 namespace SmartPeak
 {
