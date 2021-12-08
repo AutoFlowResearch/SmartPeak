8f14f1356eb78aaa5490ef53f96170b793430aae
bertrandboudaud <bertrand.boudaud@gmail.com>
2021-12-08 11:43:38 +0100

    Set hash to the ${IMGUI_VERSION} CMake variable

--- external/imgui.cmake
+++ external/imgui.cmake
@@ -3,13 +3,12 @@ include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
 set_property (DIRECTORY PROPERTY EP_BASE Dependencies)
 
 set (DEPENDENCIES)
-#set (IMGUI_VERSION 6a0d0dab5a9f0b9518a2bc9bb456a69895ae0962) # tag: v1.72b, 2019/07/31
-set (IMGUI_VERSION v1.81) # tables
+set (IMGUI_VERSION 848d21b6b56d369fb7976bd88b5f4094ab9ea50a) # on Docking branch
 message (STATUS "Adding imgui ${IMGUI_VERSION} as an external project.")
 
 ExternalProject_Add(imgui
   GIT_REPOSITORY https://github.com/ocornut/imgui.git
-  GIT_TAG 848d21b6b56d369fb7976bd88b5f4094ab9ea50a
+  GIT_TAG ${IMGUI_VERSION}
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
