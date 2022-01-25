fd18ecf7066c72a496e13ff41926893303a4d3eb
bertrandboudaud <bertrand.boudaud@gmail.com>
2022-01-25 07:43:40 +0100

    Patching portable file dialogs for mac os / save as dialog box

--- external/portablefiledialogs.cmake
+++ external/portablefiledialogs.cmake
@@ -3,11 +3,11 @@ include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
 set_property (DIRECTORY PROPERTY EP_BASE Dependencies)
 
 set (DEPENDENCIES)
-set (PORTABLE_FILE_DIALOG_VERSION 7a7a9f5fa800b28331672742c0e774d46186e34f)
+set (PORTABLE_FILE_DIALOG_VERSION fix/macos_save_as)
 message (STATUS "Adding portable-file-dialogs ${PORTABLE_FILE_DIALOG_VERSION} as an external project.")
 
 ExternalProject_Add(portablefiledialogs
-  GIT_REPOSITORY https://github.com/samhocevar/portable-file-dialogs.git
+  GIT_REPOSITORY https://github.com/bertrandboudaud/portable-file-dialogs.git
   GIT_TAG ${PORTABLE_FILE_DIALOG_VERSION}
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
