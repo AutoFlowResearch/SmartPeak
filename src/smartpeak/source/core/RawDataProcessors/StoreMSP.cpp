6c99f70be622d4acc2882ff51a3dc6e569b3fec8
bertrandboudaud <bertrand.boudaud@gmail.com>
2022-04-27 07:32:34 +0200

    Update src/smartpeak/source/core/RawDataProcessors/StoreMSP.cpp
    Co-authored-by: Douglas McCloskey <dmccloskey@users.noreply.github.com>

--- src/smartpeak/source/core/RawDataProcessors/StoreMSP.cpp
+++ src/smartpeak/source/core/RawDataProcessors/StoreMSP.cpp
@@ -18,7 +18,7 @@
 //
 // --------------------------------------------------------------------------
 // $Maintainer: Douglas McCloskey $
-// $Authors: Douglas McCloskey, Pasquale Domenico Colaianni $
+// $Authors: Douglas McCloskey $
 // --------------------------------------------------------------------------
 #include <SmartPeak/core/RawDataProcessors/StoreMSP.h>
 #include <SmartPeak/core/Filenames.h>
